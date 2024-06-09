b5cd6b4b0c53402db3ff1208daa9df643431c792
Hendrix Polanco <20220226@itla.edu.do>
2024-06-09 18:53:40 -0400

    first commit

---
+++ inde.js
@@ -0,0 +1,6 @@
+// index.js
+function holaMundo() {
+    return "Hola Mundo, Hendrix Polanco 2022-0226";
+}
+
+module.exports = holaMundo;
---
+++ index.test.js
@@ -0,0 +1,9 @@
+// index.test.js
+const holaMundo = require('./index');
+
+test('debe retornar "Hola Mundo, Hendrix Polanco 2022-022"', () => {
+    expect(holaMundo()).toBe("Hola Mundo");
+});
+
+
+
